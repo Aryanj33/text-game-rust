dbf833e05e0ae5ea35e9f6614b2d1f29e1e84b56
Aryanj33 <aryanwuw@gmail.com>
2025-02-11 18:26:56 +0530

    fixed games.rs

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! # Kingslayer
+//!
+//! Kingslayer is a text-based adventure game and library written in Rust.
+
+mod action;
+mod container;
+mod direction;
+mod game;
+mod item;
+mod tokens;
+
+pub use game::Game;
